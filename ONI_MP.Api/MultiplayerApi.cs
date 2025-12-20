fbca0298db0c49fe5cf0f4a8225f3bc3e846f2f5
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2025-12-20 16:58:36 +0000

    PLib nuget import

--- ONI_MP.Api/MultiplayerApi.cs
+++ ONI_MP.Api/MultiplayerApi.cs
@@ -1,7 +1,16 @@
-﻿namespace ONI_MP.Api
-{
-    public class MultiplayerApi
-    {
+﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Core;
 
+namespace ONI_MP.Api
+{
+    public class MultiplayerApi : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+
+            PUtil.InitLibrary(true);
+        }
     }
 }
