42468df34fe5fc473280395d8a784fed0bddd696
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-03 13:51:24 +0100

    Synchronize custom names

---
+++ ClassLibrary1/Networking/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using ONI_MP.Networking.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ONI_MP.Networking
+{
+	public static class Extensions
+	{
+		public static NetworkIdentity GetNetIdentity(this KMonoBehaviour behaviour)
+		{
+			if (behaviour.IsNullOrDestroyed() || behaviour.gameObject.IsNullOrDestroyed())
+			{
+				return null;
+			}
+			return behaviour.gameObject.GetNetIdentity();
+		}
+		public static NetworkIdentity GetNetIdentity(this GameObject go)
+		{
+			if (go.IsNullOrDestroyed())
+			{
+				return null;
+			}
+
+			if (go.TryGetComponent<NetworkIdentity>(out var identity))
+				return identity;
+
+			return go.AddComponent<NetworkIdentity>();
+		}
+		public static int GetNetId(this KMonoBehaviour behaviour)
+		{
+			var identity = GetNetIdentity(behaviour);
+			if (identity == null)
+				return 0;
+			return identity.NetId;
+		}
+	}
+}
