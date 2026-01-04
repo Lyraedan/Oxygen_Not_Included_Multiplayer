6c3a3bc74841ca3125a11e6e2b9432073554f5ce
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-04 05:09:49 +0100

    ComplexFabricator product spawn

---
+++ ClassLibrary1/Networking/Packets/World/Buildings/ComplexFabricatorSpawnProductPacket.cs
@@ -0,0 +1,43 @@
+﻿using ONI_MP.Networking.Packets.Architecture;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONI_MP.Networking.Packets.World.Buildings
+{
+	internal class ComplexFabricatorSpawnProductPacket : IPacket
+	{
+		public int NetId, CompletedRecipeIdx;
+		public ComplexFabricatorSpawnProductPacket() { }
+		public ComplexFabricatorSpawnProductPacket(ComplexFabricator cf)
+		{
+			NetId = cf.GetNetId();
+			CompletedRecipeIdx = cf.CurrentOrderIdx;
+		}
+		public void Serialize(BinaryWriter writer)
+		{
+			writer.Write(NetId);
+			writer.Write(CompletedRecipeIdx);
+		}
+
+		public void Deserialize(BinaryReader reader)
+		{
+			NetId = reader.ReadInt32();
+			CompletedRecipeIdx = reader.ReadInt32();
+		}
+
+
+		public void OnDispatched()
+		{
+			if(NetworkIdentityRegistry.TryGetComponent<ComplexFabricator>(NetId, out var fab))
+			{
+				return;
+			}
+			ComplexRecipe complexRecipe = fab.recipe_list[CompletedRecipeIdx];
+			fab.SpawnOrderProduct(complexRecipe);
+		}
+	}
+}
---
+++ ClassLibrary1/Patches/World/Buildings/ComplexFabricator_Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using ONI_MP.Networking;
+using ONI_MP.Networking.Packets.World.Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONI_MP.Patches.World.Buildings
+{
+	internal class ComplexFabricator_Patches
+	{
+
+        [HarmonyPatch(typeof(ComplexFabricator), nameof(ComplexFabricator.SpawnOrderProduct))]
+        public class ComplexFabricator_SpawnOrderProduct_Patch
+        {
+            public static void Postfix(ComplexFabricator __instance)
+			{
+				if (!MultiplayerSession.InSession || !MultiplayerSession.IsHost)
+					return;
+
+				PacketSender.SendToAllClients(new ComplexFabricatorSpawnProductPacket(__instance));
+			}
+        }
+	}
+}
