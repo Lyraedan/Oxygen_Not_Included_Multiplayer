a3a301903c133339e140b4db53310057065f38f5
Cappecasper03 <03repsac@gmail.com>
2026-01-06 16:34:43 +0100

    Simple working attack tool

---
+++ ClassLibrary1/Networking/Packets/Tools/Attack/AttackToolPacket.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using HarmonyLib;
+using ONI_MP.DebugTools;
+using ONI_MP.Networking.Packets.Architecture;
+using Steamworks;
+using UnityEngine;
+
+namespace ONI_MP.Networking.Packets.Tools.Attack;
+
+public class AttackToolPacket : IPacket
+{
+    private CSteamID        SenderId = MultiplayerSession.LocalSteamID;
+    private Vector2         Min;
+    private Vector2         Max;
+    private PrioritySetting Priority = ToolMenu.Instance.PriorityScreen.GetLastSelectedPriority();
+
+    public AttackToolPacket()
+    {
+    }
+
+    public AttackToolPacket(Vector2 min, Vector2 max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    public void Serialize(BinaryWriter writer)
+    {
+        writer.Write(SenderId.m_SteamID);
+        writer.Write(Min);
+        writer.Write(Max);
+        writer.Write((int)Priority.priority_class);
+        writer.Write(Priority.priority_value);
+    }
+
+    public void Deserialize(BinaryReader reader)
+    {
+        SenderId = new CSteamID(reader.ReadUInt64());
+        Min      = reader.ReadVector2();
+        Max      = reader.ReadVector2();
+        Priority = new PrioritySetting((PriorityScreen.PriorityClass)reader.ReadInt32(), reader.ReadInt32());
+    }
+
+    public void OnDispatched()
+    {
+        Traverse        lastSelectedPriority = Traverse.Create(ToolMenu.Instance.PriorityScreen).Field("lastSelectedPriority");
+        PrioritySetting prioritySetting      = lastSelectedPriority.GetValue<PrioritySetting>();
+
+        lastSelectedPriority.SetValue(Priority);
+
+        AttackTool.MarkForAttack(Min, Max, true);
+
+        lastSelectedPriority.SetValue(prioritySetting);
+
+        if (!MultiplayerSession.IsHost)
+            return;
+
+        var exclude = new HashSet<CSteamID>
+        {
+            SenderId,
+            MultiplayerSession.LocalSteamID
+        };
+        PacketSender.SendToAllExcluding(this, exclude);
+        DebugConsole.Log($"[AttackToolPacket] Host rebroadcasted MarkForAttack({Min}, {Max}, true)");
+    }
+}
---
+++ ClassLibrary1/Patches/ToolPatches/Attack/AttackToolPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using ONI_MP.DebugTools;
+using ONI_MP.Networking;
+using ONI_MP.Networking.Packets.Tools.Attack;
+using UnityEngine;
+
+namespace ONI_MP.Patches.ToolPatches.Attack;
+
+[HarmonyPatch(typeof(AttackTool), nameof(AttackTool.OnDragComplete))]
+public class AttackToolPatch
+{
+    private static void Postfix(Vector3 downPos, Vector3 upPos, AttackTool __instance)
+    {
+        if (!MultiplayerSession.InSession)
+            return;
+
+        Traverse get_regularized_pos = Traverse.Create(__instance).Method("GetRegularizedPos", [typeof(Vector2), typeof(bool)]);
+
+        object min_object = get_regularized_pos?.GetValue(Vector2.Min(downPos, upPos), true);
+        object max_object = get_regularized_pos?.GetValue(Vector2.Max(downPos, upPos), false);
+
+        if (min_object == null || max_object == null)
+            return;
+
+        AttackToolPacket packet = new AttackToolPacket((Vector2)min_object, (Vector2)max_object);
+
+        if (MultiplayerSession.IsHost)
+            PacketSender.SendToAllClients(packet);
+        else
+            PacketSender.SendToHost(packet);
+    }
+}
