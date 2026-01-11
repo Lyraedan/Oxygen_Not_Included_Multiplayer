19ba2a4ed1111ebbf12a2c7f4111196004a837bc
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-11 15:59:48 +0100

    Farter dupe prevent farting in printing pod screen

---
+++ ClassLibrary1/Patches/Duplicant/Flatulence_Patch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ONI_MP.Patches.Duplicant
+{
+	internal class Flatulence_Patch
+	{
+
+		[HarmonyPatch(typeof(Flatulence), nameof(Flatulence.Emit))]
+		public class Flatulence_Emit_Patch
+		{
+			/// <summary>
+			/// Skip farting for printing pod preview duplicants
+			/// </summary>
+			/// <param name="data"></param>
+			/// <returns></returns>
+			public static bool Prefix(object data)
+			{
+				GameObject gameObject = (GameObject)data;
+				return (gameObject.PrefabID() != GameTags.MinionSelectPreview);
+			}
+		}
+	}
+}
