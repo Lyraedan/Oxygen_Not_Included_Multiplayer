c2d295976e4a9ed90d451085b2f48d5f39e7a898
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-11 20:58:57 +0100

    skip ColonyDiagnostic evaluation on cliuent

---
+++ ClassLibrary1/Networking/Packets/World/ColonyDiagnostic_Patches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONI_MP.Networking.Packets.World
+{
+	internal class ColonyDiagnostic_Patches
+	{
+
+        [HarmonyPatch(typeof(ColonyDiagnostic), nameof(ColonyDiagnostic.Evaluate))]
+        public class ColonyDiagnostic_Evaluate_Patch
+        {
+            public static bool Prefix(ColonyDiagnostic __instance) => !MultiplayerSession.IsClient;
+        }
+	}
+}
