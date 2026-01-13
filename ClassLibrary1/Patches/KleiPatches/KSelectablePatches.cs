245629b72b27fb8f123dbbc64c6ae2c9c05475f3
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-01-13 01:19:22 +0000

    Started work in syncing Status items

---
+++ ClassLibrary1/Patches/KleiPatches/KSelectablePatches.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using ONI_MP.Networking;
+using ONI_MP.Networking.Components;
+
+namespace ONI_MP.Patches.KleiPatches
+{
+    public class KSelectablePatches
+    {
+        public static Dictionary<StatusItemGroup, NetworkIdentity> SelectableStatusItemGroupToNetIdentity = new Dictionary<StatusItemGroup, NetworkIdentity>();
+
+        [HarmonyPatch(typeof(KSelectable), nameof(KSelectable.OnPrefabInit))]
+        public static class KSelectable_OnPrefabInit_Patch
+        {
+            public static void Postfix(KSelectable __instance)
+            {
+                SelectableStatusItemGroupToNetIdentity.Add(__instance.statusItemGroup, __instance.GetNetIdentity());
+            }
+        }
+    }
+}
